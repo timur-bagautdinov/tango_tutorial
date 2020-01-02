b6fc9da5936f7cdc7f204b01c877d655e2a36ed1
timur-bagautdinov <timur.bagautdinov@gmail.com>
2020-01-02 21:28:21 +0300

    Simple template

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Rango says hey there partner! <br/> <a href="/rango/about/">About</a>')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
