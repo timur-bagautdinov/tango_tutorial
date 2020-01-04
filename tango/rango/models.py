f94134cebc0386d6f53afae8d917f73d9d72ca6a
timur-bagautdinov <timur.bagautdinov@gmail.com>
2020-01-04 22:51:43 +0300

    Add slug field to the Category model. Add migration script

--- tango/rango/models.py
+++ tango/rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 # Create your models here.
@@ -6,6 +7,12 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.slug = slugify(self.name)
+        super().save(force_insert, force_update, using, update_fields)
 
     class Meta:
         verbose_name_plural = 'Categories'
