8ab52f43a58748c573fc64b550041804838c6436
timur-bagautdinov <timur.bagautdinov@gmail.com>
2020-01-04 18:35:12 +0300

    Showing Categories in the main page

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from rango.models import Category
 
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {'categories': category_list}
 
     return render(request, 'rango/index.html', context=context_dict)
 
