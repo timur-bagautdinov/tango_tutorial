b249a1ff66855efdb0e4afb976acb96817a05077
timur-bagautdinov <timur.bagautdinov@gmail.com>
2020-01-09 22:50:37 +0300

    Add forms for Category and Page. Add views for adding Category.

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rango.forms import CategoryForm
 from rango.models import Category, Page
 
 
@@ -33,3 +34,18 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
